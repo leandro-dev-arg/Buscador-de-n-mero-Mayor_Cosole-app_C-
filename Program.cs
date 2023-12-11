b4679902986ee495ce39f8e8096cb8c564727b65
leandro <leandro.dev.arg@gmail.com>
2023-12-11 17:07:39 -0300

    subida inicial

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace U3_programa4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { float n1,n2,n3,n4;
+            Console.WriteLine("ingrese un número por favor");
+            n1=float.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro número por favor");
+            n2=float.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese otro número por favor");
+            n3=float.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el último número por favor");
+            n4=float.Parse(Console.ReadLine());
+
+            if (n1>n2 && n1>n3 && n1>n4 )
+            {
+                Console.WriteLine( n1 +" es el mayor");
+            }else if(n2>n1 && n2>n3 && n2>n4 ){
+                Console.WriteLine( n2 +" es el mayor");
+            }else if(n3>n1 && n3>n2 && n3>n4){
+                Console.WriteLine( n3 +" es el mayor");
+            }else{
+                Console.WriteLine( n4 +" es el mayor");
+            }
+
+        }
+    }
+}
